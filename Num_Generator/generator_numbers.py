098eab7639519fa5746084bef34807b81440304f
Nidan6 <108980863+Nidan6@users.noreply.github.com>
2024-09-29 16:55:56 +0300

    Add files via upload

---
+++ Num_Generator/generator_numbers.py
@@ -0,0 +1,19 @@
+import re
+
+def generator_numbers(text: str):
+    filter = r"\d+\.*\d+"
+    numbers = re.findall(filter, text)
+    for number in numbers:
+        yield number
+
+def sum_profit(text: str, func):
+    summ = 0
+    for number in func(text):
+        summ += float(number)
+    return summ
+
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
