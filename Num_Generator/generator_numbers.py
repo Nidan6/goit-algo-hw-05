2386e07677ec0190b3f5eb2f31ff23f7b7563166
Nidan6 <108980863+Nidan6@users.noreply.github.com>
2024-10-02 17:35:17 +0300

    Update generator_numbers.py

--- Num_Generator/generator_numbers.py
+++ Num_Generator/generator_numbers.py
@@ -1,19 +1,25 @@
 import re
 
 def generator_numbers(text: str):
-    filter = r"\d+\.*\d+"
+    filter = r"\b\d+\.*\d+\b"
     numbers = re.findall(filter, text)
     for number in numbers:
-        yield number
+        try:
+            yield float(number)
+        except ValueError:
+            continue
 
 def sum_profit(text: str, func):
     summ = 0
     for number in func(text):
-        summ += float(number)
+        try:
+            summ += float(number)
+        except ValueError:
+            continue
     return summ
 
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів. 1.2.3"
 
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
